09dee83efedb70296dba063f6d6e6a2bc4e0cfb4
Anna Bagrowska <anna_bagrowska@wp.pl>
2019-02-24 11:48:05 +0100

    add directive and shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BackgroundColorDirective } from './background-color.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BackgroundColorDirective
   ],
   imports: [
     BrowserModule
